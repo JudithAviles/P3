5da03e28e7f11f6578ab4e328001de4c823a90f3
tomaspr <tomas.parramon@estudiantat.upc.edu>
2026-05-17 18:25:19 +0200

    canviat el path perque ara funcioni per tothom

--- scripts/run_get_pitch.sh
+++ scripts/run_get_pitch.sh
@@ -2,7 +2,9 @@
 
 set -o pipefail
 
-export PATH="/home/tomas/PAV/bin:$PATH"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+export PATH="$PROJECT_DIR/build/release/src/get_pitch:$PATH"
 
 GETF0="get_pitch"
 
